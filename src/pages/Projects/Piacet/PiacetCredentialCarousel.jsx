336f8ad5de13631327e11469c5a561585546b0fc
Nacho Lorenzo <nacholorenzo.650@gmail.com>
2024-07-12 18:47:43 -0300

    animation in progress

--- src/pages/Projects/Piacet/PiacetCredentialCarousel.jsx
+++ src/pages/Projects/Piacet/PiacetCredentialCarousel.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import './PiacetStyles.scss'
 
 const credentialTypes = {
@@ -8,32 +9,83 @@ const credentialTypes = {
     covid: {label: 'Covid', color: '#00B6CD'},
     other: {label: 'Other', color: '#9870F1'}
 }
+const initialCreds = [
+    {
+        title: 'Cardiology Technician',
+        category: credentialTypes.specialty,
+        expiry: '6 Jun 2027',
+        created: '28 Feb 2023',
+        id: '0785761235-56'
+    },
+    {
+        title: 'Cardiology Technician 2',
+        category: credentialTypes.covid,
+        expiry: '6 Jun 2027',
+        created: '28 Feb 2023',
+        id: '9167494-81'
+    },
+    {
+        title: 'Cardiology Technician 3',
+        category: credentialTypes.educational,
+        expiry: '6 Jun 2027',
+        created: '28 Feb 2023',
+        id: '8949421-11'
+    },
+    {
+        title: 'Cardiology Technician 4',
+        category: credentialTypes.other,
+        expiry: '6 Jun 2027',
+        created: '28 Feb 2023',
+        id: '00173461-92'
+    },
+    {
+        title: 'Cardiology Technician 5',
+        category: credentialTypes.clinical,
+        expiry: '6 Jun 2027',
+        created: '28 Feb 2023',
+        id: '7910281-04'
+    },
+    {
+        title: 'Cardiology Technician 6',
+        category: credentialTypes.clinical,
+        expiry: '6 Jun 2027',
+        created: '28 Feb 2023',
+        id: '99472166-39'
+    },
+]
 
 const PiacetCredentialsCarousel = () => {
-    const credentials = [
-        {
-            title: 'Cardiology Technician',
-            category: credentialTypes.specialty,
-            expiry: '6 Jun 2027',
-            created: '28 Feb 2023',
-            id: '0785761235-56'
-        },
-    ]
+    const [credentials, setCredentials] = useState(initialCreds)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            let newCreds = [...credentials]
+            const lastCred = newCreds.pop()
+            newCreds.unshift(lastCred)
+            setCredentials(newCreds)
+          }, 5500)
+      
+          return () => clearInterval(interval)
+    }, [credentials])
 
     return (
         <div className="credentials-wrapper">
             {credentials.map((cred, index) => {
                 return (
-                    <div className="credential-container">
+                    <div key={index + cred.id} className="credential-container" style={{ top: `${index * 80}px`, zIndex: 100 - index, transform: `scale(${1 - index/60})`}}>
                         <div className="header">
                             <div className="wrap">
                                 <div className={`title hide-shimmer`}>
                                     {cred.title}
-                                    <div className="shimmer-box" style={{transitionDelay: '4s'}}/>
+                                    <div className="shimmer-box" style={{animationDelay: '1.0s'}}>
+                                        <div className="shimmer-element"/>
+                                    </div>
                                 </div>
                                 <div className={`category hide-shimmer`} style={{color: cred.category.color}}>
                                     {cred.category.label}
-                                    <div className="shimmer-box" style={{transitionDelay: '4.4s'}}/>
+                                    <div className="shimmer-box" style={{animationDelay: '1.4s'}}>
+                                        <div className="shimmer-element"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -41,21 +93,29 @@ const PiacetCredentialsCarousel = () => {
                             <div className="item">
                                 <div className={`label hide-shimmer`}>
                                     Created
-                                    <div className="shimmer-box" style={{transitionDelay: '4.8s'}}/>
+                                    <div className="shimmer-box" style={{animationDelay: '1.8s'}}>
+                                        <div className="shimmer-element"/>
+                                    </div>
                                 </div>
                                 <div className={`value hide-shimmer`}>
                                     {cred.created}
-                                    <div className="shimmer-box" style={{transitionDelay: '5.4s'}}/>
+                                    <div className="shimmer-box" style={{animationDelay: '2.4s'}}>
+                                        <div className="shimmer-element"/>
+                                    </div>
                                 </div>
                             </div>
                             <div className="item">
                                 <div className={`label hide-shimmer`}>
                                     Expiry
-                                    <div className="shimmer-box" style={{transitionDelay: '5.8s'}}/>
+                                    <div className="shimmer-box" style={{animationDelay: '2.8s'}}>
+                                        <div className="shimmer-element"/>
+                                    </div>
                                 </div>
                                 <div className={`value hide-shimmer`}>
                                     {cred.expiry}
-                                    <div className="shimmer-box" style={{transitionDelay: '6.4s'}}/>
+                                    <div className="shimmer-box" style={{animationDelay: '3.4s'}}>
+                                        <div className="shimmer-element"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -63,11 +123,15 @@ const PiacetCredentialsCarousel = () => {
                             <div className="item">
                                 <div className={`label hide-shimmer`}>
                                     ID Number
-                                    <div className="shimmer-box" style={{transitionDelay: '7s'}}/>
+                                    <div className="shimmer-box" style={{animationDelay: '4.0s'}}>
+                                        <div className="shimmer-element"/>
+                                    </div>
                                 </div>
                                 <div className={`value hide-shimmer`}>
                                     {cred.id}
-                                    <div className="shimmer-box" style={{transitionDelay: '7.4s'}}/>
+                                    <div className="shimmer-box" style={{animationDelay: '4.4s'}}>
+                                        <div className="shimmer-element"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
